9b24f4ee908017a5dbf8815f900e81ee92e74411
ken <yash20004x17@gmail.com>
2025-07-27 11:32:51 +0000

    10 pattern problems solved

--- src/index.ts
+++ src/index.ts
@@ -66,19 +66,42 @@
 // }
 // pattern5(5);
 
-// let val = "";
+// let val1 = "";
 // function pattern6(num: number): void {
 //   for (let i = 0; i < num; i++) {
 //     //   space
 //     for (let j = 0; j < num - i - 1; j++) {
-//       val += " ";
+//       val1 += " ";
 //     }
 //     //   star
 //     for (let j = 0; j < 2 * i +  1; j++) {
-//       val += "*";
+//       val1 += "*";
 //     }
 //     //   space
 //     for (let j = 0; j < num - i - 1; j++) {
+//       val1 += " ";
+//     }
+//     console.log(val1);
+//     val1 = "";
+//     console.log("\n");
+//   }
+// }
+
+// pattern6(5);
+
+// let val = "";
+// function pattern7(num: number): void {
+//   for (let i = 0; i < num; i++) {
+//     //   space
+//     for (let j = 0; j < i; j++) {
+//       val += " ";
+//     }
+//     //   star
+//     for (let j = 0; j < 2 * num - (2 * i + 1); j++) {
+//       val += "*";
+//     }
+//     //   space
+//     for (let j = 0; j < i; j++) {
 //       val += " ";
 //     }
 //     console.log(val);
@@ -87,27 +110,107 @@
 //   }
 // }
 
-// pattern6(5);
+// pattern7(5);
+
+// let val = "";
+// function pattern8(num: number): void {
+//   for (let i = 0; i < 2*num; i++) {
+//     //   space
+//     let stars = i
+//     if(stars > num) stars = 2*num - i
+//     for (let j = 0; j < stars; j++) {
+//       val += "*";
+//     }
+//     console.log(val);
+//     val = "";
+//     console.log("\n");
+//   }
+// }
+// pattern8(5)
+
+// let val = "";
+// let start;
+// function pattern8(num: number): void {
+//   for (let i = 0; i < num; i++) {
+//     //   space
+//     if (i % 2 == 0) {
+//       start = 1;
+//     } else {
+//       start = 0;
+//     }
+//     for (let j = 0; j <= i; j++) {
+//       val += start;
+//       start = 1 - start;
+//     }
+//     console.log(val);
+//     val = "";
+//     console.log("\n");
+//   }
+// }
+// pattern8(5);
+
+// let val = "";
+// function pattern9(num: number): void {
+//   for (let i = 1; i < num; i++) {
+//     // number
+//     for (let j = 1; j < i; j++) {
+//       val += j ;
+//     }
+//     // space
+//     for (let j = 1; j < 2 * num - (2 * i - 2); j++) {
+//       val += " ";
+//     }
+//     // number
+//     for (let j = i; j >= i; j--) {
+//       val += j ;
+//     }
+//     console.log(val);
+//     val = "";
+//     console.log("\n");
+//   }
+// }
+// pattern9(5);
+
+// let val1 = "";
+// function pattern1(num: number) {
+//   for (let i = 0; i < num; i++) {
+//     // space
+//     for (let j = 0; j < i; j++) {
+//       val += "|";
+//     }
+//     // stars
+//     for (let j = 0; j < 2 * num - (2 * i + 1); j++) {
+//       val += "*";
+//     }
+//     // space
+//     for (let j = 0; j < i; j++) {
+//       val += "|";
+//     }
+
+//     console.log(val);
+//     val = "";
+//     console.log("\n");
+//   }
+// }
+
+// pattern1(5);
 
 let val = "";
-function pattern7(num: number): void {
-  for (let i = 0; i < num; i++) {
-    //   space
-    for (let j = num; j < num - i - 1; j--) {
-      val += " ";
+function pattern(num: number) {
+  for (let i = 0; i <= 2 * num - 1; i++) {
+    // stars
+    let loop = i;
+    if (loop > num) {
+      loop = 2* num - i;
     }
-    //   star
-    for (let j = num; j < num * 2 - 1; j--) {
-      val += "*";
+    for (let j = 0; j < loop; j++) {
+      val += "* ";
     }
-    //   space
-    for (let j = num; j < num - i - 1; j--) {
-      val += " ";
-    }
+
     console.log(val);
     val = "";
     console.log("\n");
   }
 }
 
-pattern7(5);
+pattern(5);
